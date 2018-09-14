54fae79c21a8ad1d0d2dcadec7d3f3748f77bed9
David McGrann <dpmcgrann@gmail.com>
2018-09-14 20:14:05 +0000

    basic user controller setup

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,2 +1,13 @@
 class GoalsController < ApplicationController
+  
+  get '/goals' do
+    if logged_in?
+      @user = current_user
+      @goals = Goal.all
+      erb :"/goals/goals"
+    else
+      redirect '/login'
+    end
+  end
+  
 end
--- app/controllers/users_controllers.rb
+++ app/controllers/users_controllers.rb
@@ -1,2 +1,47 @@
 class UsersController < ApplicationController
+  
+  get "/signup" do
+    if !logged_in?
+      erb :'users/create_user'
+    else
+     redirect '/goals'
+    end
+  end
+
+  post "/signup" do
+    if params[:username] == "" || params[:email] == "" || params[:password] == ""
+      redirect '/signup'
+    else
+      @user = User.create(params)
+      session[:user_id] = @user.id
+      redirect to '/goals'
+    end
+  end
+
+  get "/login" do
+    if !logged_in?
+      erb :'/users/login'
+    else
+     redirect to '/goals'
+    end
+  end
+
+  post "/login" do
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect to "/goals"
+    else
+      redirect to "/signup"
+    end
+  end
+
+  get "/logout" do
+    if logged_in?
+      session.clear
+      redirect "/login"
+    else
+      redirect "/"
+    end
+  end
 end
