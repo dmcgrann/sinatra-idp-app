9dd533eb6eed24794a2b78ef78682d324a2dafee
David McGrann <dpmcgrann@gmail.com>
2018-09-19 22:57:21 +0000

    added edit objective controllers

--- app/controllers/objectives_controller.rb
+++ app/controllers/objectives_controller.rb
@@ -24,6 +24,29 @@ class ObjectivesController < ApplicationController
     redirect to "/objectives"
   end
   
+  get '/objectives/:id' do
+    if logged_in?
+      @objective = Objective.find_by_id(params[:id])
+      erb :"/objectives/show_objective"
+    else
+      redirect '/login'
+    end
+  end
   
+  get '/objectives/:id/edit' do
+    if !logged_in?
+      redirect '/login'
+    else
+      @objective = Objective.find_by_id(params[:id])
+      erb :'/objectives/edit_objective'
+    end
+  end
+  
+  patch '/objectives/:id' do
+    @objective = Objective.find_by_id(params[:id])
+    @objective.update(content: params[:content], deadline: params[:deadline])
+    @objective.save
+    redirect "/objectives/#{@objective.id}"
+  end
   
 end
